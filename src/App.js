a1fa33715ecaed402555ebe70d56048895dc4ade
Rockielitte <truongquang2121@gmail.com>
2022-10-09 22:59:45 +0700

    congif route and layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoute } from './routes';
+import { DefaultLayout } from './components/Layout';
+
 function App() {
-    return <div>App</div>;
+    return (
+        <Router>
+            <div>App</div>
+            <Routes>
+                {publicRoute.map((route, index) => {
+                    let Layout = DefaultLayout;
+                    if (route.layout) {
+                        Layout = route.layout;
+                    } else if (route.layout === null) {
+                        Layout = Fragment;
+                    }
+
+                    const Page = route.component;
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
